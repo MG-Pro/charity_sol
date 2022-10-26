887aac223d4698899d71d35f14dd5a634a55ed2f
mg <mg84@bk.ru>
2022-10-26 19:27:26 +0300

    ad script

--- programs/charity_sol/src/lib.rs
+++ programs/charity_sol/src/lib.rs
@@ -7,38 +7,184 @@ pub mod charity_sol {
     use super::*;
     use anchor_lang::solana_program::entrypoint::ProgramResult;
 
-    pub fn initialize(
-        ctx: Context<Initialize>,
+    const SETTINGS_PDA_SEED: &[u8] = b"settings";
+    const TOP_PLATFORM_DONATORS_PDA_SEED: &[u8] = b"top_platform_donators";
+    const MAX_TOP_DONATORS_CAMPAIGN: u16 = 10;
+    const MAX_TOP_DONATORS_PLATFORM: u16 = 100;
+
+    pub fn create_app(
+        ctx: Context<CreateApp>,
         duration: u16,
         owner_commission: u16,
         reward: u64,
         exemption_limit: u64,
-        closing_limit: u64,
     ) -> ProgramResult {
-        let donation = &mut ctx.accounts.donation;
-        donation.duration = duration;
-        donation.owner_commission = owner_commission;
-        donation.reward = reward;
-        donation.exemption_limit = exemption_limit;
-        donation.closing_limit = closing_limit;
+        let settings = &mut ctx.accounts.settings;
+        settings.platform_owner = ctx.accounts.platform_owner.key();
+        settings.reward_period = duration;
+        settings.owner_fee = owner_commission;
+        settings.reward = reward;
+        settings.exemption_fee_limit = exemption_limit;
+        settings.max_top_donators_campaign = MAX_TOP_DONATORS_CAMPAIGN;
+        settings.max_top_donators_platform = MAX_TOP_DONATORS_PLATFORM;
+
+        let top_platform_donators = &mut ctx.accounts.top_platform_donators;
+        top_platform_donators.donators = vec![];
+
         Ok(())
     }
-}
 
-#[derive(Accounts)]
-pub struct Initialize<'info> {
-    #[account(init, payer = owner, space = 64 + 64)]
-    pub donation: Account<'info, Donation>,
-    #[account(mut)]
-    pub owner: Signer<'info>,
-    pub system_program: Program<'info, System>,
-}
+    pub fn init_campaign(ctx: Context<InitCampaign>) -> ProgramResult {
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.campaign_owner = ctx.accounts.campaign_owner.key();
+        campaign.top_donators_campaign = vec![];
+        campaign.fund = 0;
 
-#[account]
-pub struct Donation {
-    pub duration: u16,
-    pub owner_commission: u16,
-    pub reward: u64,
-    pub exemption_limit: u64,
-    pub closing_limit: u64,
+        Ok(())
+    }
+
+    pub fn donate(ctx: Context<Donate>, amount: u64) -> ProgramResult {
+        let settings = &mut ctx.accounts.settings;
+
+        msg!("amount ------------{}", amount);
+        msg!("owner_commission ------------{}", settings.owner_fee);
+
+        let instruction = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.donator.key(),
+            &ctx.accounts.campaign.key(),
+            amount,
+        );
+
+        anchor_lang::solana_program::program::invoke(
+            &instruction,
+            &[
+                ctx.accounts.donator.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ],
+        )
+        .expect("");
+
+        settings.owner_fee = 10;
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.fund += amount;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Donate>, amount: u64) -> ProgramResult {
+        let settings = &mut ctx.accounts.settings;
+        let owner_part: u64 = amount * u64::from(settings.owner_fee) / 100;
+
+        let instruction = anchor_lang::solana_program::system_instruction::transfer(
+            &ctx.accounts.donator.key(),
+            &ctx.accounts.campaign.key(),
+            amount,
+        );
+
+        anchor_lang::solana_program::program::invoke(
+            &instruction,
+            &[
+                ctx.accounts.donator.to_account_info(),
+                ctx.accounts.campaign.to_account_info(),
+            ],
+        )
+        .expect("");
+
+        let campaign = &mut ctx.accounts.campaign;
+        campaign.fund += amount;
+        Ok(())
+    }
+
+    #[derive(Accounts)]
+    pub struct CreateApp<'info> {
+        #[account(mut)]
+        pub platform_owner: Signer<'info>,
+
+        #[account(
+            init,
+            payer = platform_owner,
+            space = 64 + 64,
+            seeds = [&SETTINGS_PDA_SEED],
+            bump
+        )]
+        pub settings: Account<'info, Settings>,
+
+        #[account(
+            init,
+            payer = platform_owner,
+            space = 64 + 64,
+            seeds = [&TOP_PLATFORM_DONATORS_PDA_SEED],
+            bump
+        )]
+        pub top_platform_donators: Account<'info, TopDonators>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+    pub struct InitCampaign<'info> {
+        #[account(mut)]
+        pub campaign_owner: Signer<'info>,
+        #[account(
+            init,
+            payer = campaign_owner,
+            space = 64 + 64,
+            seeds = [b"campaign", campaign_owner.key().as_ref()],
+            bump
+        )]
+        pub campaign: Account<'info, Campaign>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+    pub struct Donate<'info> {
+        #[account(mut)]
+        pub campaign: Account<'info, Campaign>,
+        #[account(mut)]
+        pub donator: Signer<'info>,
+        #[account()]
+        pub settings: Account<'info, Settings>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[derive(Accounts)]
+    pub struct Withdraw<'info> {
+        #[account(mut)]
+        pub campaign_owner: Signer<'info>,
+        #[account(mut)]
+        pub platform_owner: AccountInfo<'info>,
+        #[account(mut)]
+        pub campaign: Account<'info, Campaign>,
+        #[account()]
+        pub settings: Account<'info, Settings>,
+        pub system_program: Program<'info, System>,
+    }
+
+    #[account]
+    pub struct Campaign {
+        pub campaign_owner: Pubkey,
+        pub fund: u64,
+        pub active: bool,
+        pub top_donators_campaign: Vec<Donator>,
+    }
+
+    #[account]
+    pub struct TopDonators {
+        pub donators: Vec<Donator>,
+    }
+
+    #[account]
+    pub struct Settings {
+        pub platform_owner: Pubkey,
+        pub reward_period: u16,
+        pub owner_fee: u16,
+        pub reward: u64,
+        pub exemption_fee_limit: u64,
+        pub max_top_donators_platform: u16,
+        pub max_top_donators_campaign: u16,
+    }
+
+    #[derive(AnchorSerialize, AnchorDeserialize, Clone, Copy)]
+    pub struct Donator {
+        pub address: Pubkey,
+        pub amount: u64,
+    }
 }
--- scripts/start.ts
+++ scripts/start.ts
@@ -4,65 +4,175 @@ import {AnchorProvider, BN, Idl, Program, Wallet} from '@project-serum/anchor'
 import fs from 'fs'
 import path from 'path'
 
-const INIT_OPTS = {
-  duration: 10,
-  ownerCommission: 20,
-  reward: new BN(30),
-  exemptionLimit: new BN(100),
-  closingLimit: new BN(1000),
-}
+class Common {
+  private readonly connection: Connection
+  protected program: Program
+  protected wallet: Wallet
+  protected provider: AnchorProvider
 
-function readKeypairFromPath(path: string): Keypair {
-  try {
-    const data = JSON.parse(fs.readFileSync(path, 'utf-8'))
-    return Keypair.fromSecretKey(Buffer.from(data))
-  } catch (e) {
-    return null
+  constructor(connection: Connection, walletKeypair: Keypair) {
+    this.connection = connection
+    this.initProgram(walletKeypair)
+  }
+
+  public async getBalance(): Promise<number> {
+    return (await this.connection.getBalance(this.wallet.publicKey)) / LAMPORTS_PER_SOL
+  }
+
+  public async requestAirdrop(sol = 5) {
+    return await this.connection.requestAirdrop(this.wallet.publicKey, LAMPORTS_PER_SOL * sol)
+  }
+
+  public static readKeypairFromPath(path: string): Keypair {
+    try {
+      const data = JSON.parse(fs.readFileSync(path, 'utf-8'))
+      return Keypair.fromSecretKey(Buffer.from(data))
+    } catch (e) {
+      return null
+    }
+  }
+
+  public static writeKeypairToPath(keypair: Keypair, name: string) {
+    fs.writeFileSync(
+      path.join(__dirname, `/local-keys/${name}-keypair.json`),
+      JSON.stringify(Array.from(keypair.secretKey)),
+      {flag: 'w'},
+    )
+  }
+
+  protected async getPDA(seed: string, authority?: PublicKey): Promise<[PublicKey, number]> {
+    const seedArr = [Buffer.from(seed)]
+    if (authority) {
+      seedArr.push(authority.toBuffer())
+    }
+    return await PublicKey.findProgramAddress(
+      seedArr,
+      this.program.programId,
+    )
+  }
+
+  protected initProgram(keypair: Keypair) {
+    const programKeypair = Common.readKeypairFromPath(__dirname + '/local-keys/charity_sol-keypair.json')
+
+    this.wallet = new Wallet(keypair)
+    this.provider = new AnchorProvider(this.connection, this.wallet, {
+      preflightCommitment: 'confirmed',
+    })
+    this.program = new Program(idl as Idl, programKeypair.publicKey, this.provider)
   }
 }
 
-function writeKeypairToPath(keypair: Keypair, name: string) {
-  fs.writeFileSync(
-    path.join(__dirname, `/local-keys/${name}.json`),
-    JSON.stringify(Array.from(keypair.secretKey)),
-    {flag: 'w'},
-  )
+class Platform extends Common {
+  private readonly INIT_OPTS: Record<string, number | BN> = {
+    rewardPeriod: 10,
+    ownerFee: 20,
+    reward: new BN(30),
+    exemptionFeeLimit: new BN(100),
+  }
+
+  public async initPlatform() {
+    const [settingsPublicKey] = await this.getPDA('settings')
+    const [topPlatformDonatorsPublicKey] = await this.getPDA('top_platform_donators')
+
+    await this.program.methods.createApp(
+      this.INIT_OPTS.rewardPeriod,
+      this.INIT_OPTS.ownerFee,
+      this.INIT_OPTS.reward,
+      this.INIT_OPTS.exemptionFeeLimit,
+    )
+      .accounts({
+        platformOwner: this.wallet.publicKey,
+        topPlatformDonators: topPlatformDonatorsPublicKey,
+        settings: settingsPublicKey,
+        systemProgram: SystemProgram.programId,
+      })
+      .rpc()
+  }
+
+  public async platformInfo() {
+    const [settingsPublicKey] = await this.getPDA('settings')
+    return this.program.account.settings.fetch(settingsPublicKey)
+  }
+
+  public async getAllCampaigns(): Promise<any> {
+    return await this.program.account.campaign.all()
+  }
+
+  public async getAllSettings(): Promise<any> {
+    return await this.program.account.settings.all()
+  }
+}
+
+class Campaign extends Common {
+  public async initCampaign() {
+    const [campaignPublicKey] = await this.getPDA('campaign', this.wallet.publicKey)
+
+    const tx = this.program.methods
+      .initCampaign()
+      .accounts({
+        campaignOwner: this.wallet.publicKey,
+        campaign: campaignPublicKey,
+        systemProgram: SystemProgram.programId,
+      })
+
+
+    await tx.rpc()
+  }
+
+  public async campaignInfo() {
+    const [campaignPublicKey] = await this.getPDA('campaign', this.wallet.publicKey)
+    return this.program.account.campaign.fetch(campaignPublicKey)
+  }
+}
+
+class Donator extends Common {
+  public async donate(campaign: PublicKey, amountSol: number): Promise<void> {
+    const [settingsPublicKey] = await this.getPDA('settings')
+
+    const tx = this.program.methods
+      .donate(new BN(amountSol * LAMPORTS_PER_SOL))
+      .accounts({
+        settings: settingsPublicKey,
+        campaign,
+        donator: this.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      })
+
+
+    await tx.rpc()
+  }
 }
 
 async function start() {
-  const adminKeypair = readKeypairFromPath(__dirname + '/local-keys/admin.json')
-  const programKeypair = readKeypairFromPath(__dirname + '/local-keys/charity_sol-keypair.json')
-
   const connection = new Connection('http://localhost:8899', 'confirmed')
-  const wallet = new Wallet(adminKeypair)
-  const provider = new AnchorProvider(connection, wallet, {
-    preflightCommitment: 'confirmed',
+
+  const platformOwner = Common.readKeypairFromPath(__dirname + '/local-keys/admin.json')
+  const campaignOwner = Common.readKeypairFromPath(__dirname + '/local-keys/campaign-owner.json')
+  const donator1KP = Common.readKeypairFromPath(__dirname + '/local-keys/donator-1-keypair.json')
+
+  const platform: Platform = new Platform(connection, platformOwner)
+  const campaign: Campaign = new Campaign(connection, campaignOwner)
+  const donator1: Donator = new Donator(connection, donator1KP)
+
+  console.log('adminKeypair Balance:', await platform.getBalance())
+  console.log('campaignOwner Balance:', await campaign.getBalance())
+  console.log('donator-1 Balance:', await donator1.getBalance())
+
+  const campaigns = await platform.getAllCampaigns()
+  const settings = await platform.getAllSettings()
+  // await platform.initPlatform(platformOwner)
+  console.log(settings)
+  campaigns.forEach(c => {
+    console.log(c.account.fund.toString())
   })
 
-  // await connection.requestAirdrop(adminKeypair.publicKey, LAMPORTS_PER_SOL)
-  const adminBalance = await connection.getBalance(adminKeypair.publicKey)
 
-  console.log(adminBalance / LAMPORTS_PER_SOL)
+  // await campaign.initCampaign(campaignOwner)
+  // console.log(await campaign.campaignInfo())
 
-  const donationKeypair = Keypair.generate() // test only
-
-  const program = new Program(idl as Idl, programKeypair.publicKey, provider)
-
-  await program.methods.initialize(
-    INIT_OPTS.duration,
-    INIT_OPTS.ownerCommission,
-    INIT_OPTS.reward,
-    INIT_OPTS.exemptionLimit,
-    INIT_OPTS.closingLimit)
-    .accounts({
-      donation: donationKeypair.publicKey,
-      user: provider.wallet.publicKey,
-      systemProgram: SystemProgram.programId,
-    }).signers([donationKeypair]).rpc()
-
-
-  const account = await program.account.donation.fetch(donationKeypair.publicKey)
-  console.log(account.reward.toString())
+  // await donator1.donate(campaigns[0].publicKey, 0.01)
+  console.log(await campaign.campaignInfo())
+  console.log(await platform.platformInfo())
 }
 
 start()
